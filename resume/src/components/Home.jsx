fe4a5d5fe0b4367a02ae29dd49bfd66a167e2f75
AlexanderHolmes0 <aholme27@vols.utk.edu>
2023-07-07 14:41:12 -0400

    Package Updates

--- resume/src/components/Home.jsx
+++ resume/src/components/Home.jsx
@@ -48,7 +48,7 @@ function Home() {
 
 
 
-                <details className='detailss' open>
+                <details className='detailss' >
                     <summary className='titles'> Github Repos </summary>
                     <div className="card">
                         <div className='card-block'>
