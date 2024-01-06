ede55d6563fd73dde2308a0ee96850354c60ed76
AlexanderHolmes0 <aholme27@vols.utk.edu>
2024-01-05 21:17:44 -0500

    Update Sidebar.jsx
    New Stuffs

--- resume/src/Sidebar.jsx
+++ resume/src/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Sidebar.css';
 import { bubble as Menu } from 'react-burger-menu';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 export default function menu() {
@@ -14,56 +14,62 @@ export default function menu() {
 
   return (
     <Menu isOpen={isMenuOpen} onStateChange={({ isOpen }) => setIsMenuOpen(isOpen)} right>
-    
+
       <Link className="menu-item" to="/" onClick={handleLinkClick}> <h3><span className='grayscale'>🏡</span> Home</h3> </Link>
-    
+
       <br />
-      
-      <h3> <img src='https://www.jumpingrivers.com/blog/accessible-shiny-standards-wcag/featured.png' height='40px'></img> 
-      <a className="nodeco" target="_blank" rel="noopener noreferrer" href='https://shiny.posit.co/'> Shiny Apps</a></h3>
+
+      <h3> <img src='https://www.jumpingrivers.com/blog/accessible-shiny-standards-wcag/featured.png' height='40px'></img>
+        <a className="nodeco" target="_blank" rel="noopener noreferrer" href='https://shiny.posit.co/'> Shiny Apps</a></h3>
       <hr />
 
-      <Link className="menu-item" to="BlackJackApp" onClick={handleLinkClick}><span className='grayscale'>🃏</span>  Blackjack App   </Link>
-        
-      
-      <Link className="menu-item" to="Senate_Tracker" onClick={handleLinkClick}><span className='grayscale'>📈</span>  Senate Tracker</Link>
-        
-    
+      {/* <Link className="menu-item" to="BlackJackApp" onClick={handleLinkClick} ><span className='grayscale'>🃏</span>  Blackjack App   </Link> */}
+
+      <a className="menu-item" target="_blank" rel="noopener noreferrer" href='https://aholmes23.shinyapps.io/BlackJackApp/'> <span className='grayscale'>🃏</span>  Blackjack App </a>
+
+      <a className="menu-item" target="_blank" rel="noopener noreferrer" href='https://aholmes25.shinyapps.io/Viz_Sched_App/'> <span className='grayscale'>🗺️</span>  Sports Schedule Viz </a>
+
+      <Link className="menu-item" to="Senate_Tracker" onClick={handleLinkClick}><span className='grayscale'>🕵️‍♂️</span>  Senate Tracker</Link>
+
+
       <Link className="menu-item" to="VisualizeTree" onClick={handleLinkClick}><span className='grayscale'>🌴</span>  Visualize Tree </Link>
-        
-    
+
+
       <Link className="menu-item" to="Meta_Creator" onClick={handleLinkClick}><span className='grayscale'>🤘</span>   Meta Creator </Link>
-        
-    
+
+
       <Link className="menu-item" to="Stroke_Dash" onClick={handleLinkClick}><span className='grayscale'>🏥</span>  Stroke Dash</Link>
-      
-  
+
+
       <Link className="menu-item" to="CentralLimit" onClick={handleLinkClick}><span className='grayscale'>📊</span>  Central Limit </Link>
 
       <Link className="menu-item" to="InventorySimulation" onClick={handleLinkClick}><span className='grayscale'>🏭</span> Inventory Simulation</Link>
       <br />
-      
-      <h3> <img src='https://images.ctfassets.net/spoqsaf9291f/5M2QHtaWvuvEUU2V8MNVXH/f780593bab58709f95404d27bcb4e5d4/tableau-software.svg' height='40px'></img> 
-      <a className="nodeco" target="_blank" rel="noopener noreferrer" href='https://www.tableau.com/'> Tabelau Vizzes </a></h3>
+
+      <h3> <img src='https://images.ctfassets.net/spoqsaf9291f/5M2QHtaWvuvEUU2V8MNVXH/f780593bab58709f95404d27bcb4e5d4/tableau-software.svg' height='40px'></img>
+        <a className="nodeco" target="_blank" rel="noopener noreferrer" href='https://www.tableau.com/'> Tabelau Vizzes </a></h3>
       <hr />
       <Link className="menu-item" to="KrogerDash" onClick={handleLinkClick}><span className='grayscale'>
-      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Kroger_logo_%281961-2019%29.svg/200px-Kroger_logo_%281961-2019%29.svg.png' height='20px'></img>
-        </span>  Kroger</Link>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Kroger_logo_%281961-2019%29.svg/200px-Kroger_logo_%281961-2019%29.svg.png' height='20px'></img>
+      </span>  Kroger</Link>
       <Link className="menu-item" to="YoutubeDash" onClick={handleLinkClick}><span className='grayscale'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1024px-YouTube_full-color_icon_%282017%29.svg.png' height='20px'></img>
-        </span> Youtube</Link>
+      </span> Youtube</Link>
       <Link className="menu-item" to="KickstarterDash" onClick={handleLinkClick}><span className='grayscale'>
-      <img src='https://cdn.icon-icons.com/icons2/2429/PNG/512/kickstarter_logo_icon_147271.png' height='25px'></img>
+        <img src='https://cdn.icon-icons.com/icons2/2429/PNG/512/kickstarter_logo_icon_147271.png' height='25px'></img>
       </span> Kickstarter</Link>
       <br />
 
 
       <h3> <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvvyvK0Amsrg3C1C1EwVc9GVflA5SXQx96ygeuBOCxVg&s' height='25px'></img>
-      <a className="nodeco" target="_blank" rel="noopener noreferrer" href='https://www.kaggle.com/aholmes23'>  ML Competitions  </a></h3>
+        <a className="nodeco" target="_blank" rel="noopener noreferrer" href='https://www.kaggle.com/aholmes23'>  ML Competitions  </a></h3>
       <hr />
       <Link className="menu-item" to="RaceXGBS" onClick={handleLinkClick}><span className='grayscale'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/6/69/XGBoost_logo.png' height='25px'></img>
-        </span>  Race Models </Link>
+      </span>  Race Models </Link>
+
+      <a className="menu-item" target="_blank" rel="noopener noreferrer" href='https://drive.google.com/file/d/1T_eeD9OpME3b0wlAO_3szS0-mA6MK2Ua/view?usp=sharing'> <span className='grayscale'>📃</span>  Pakistani Used Car Sales </a>
+
     </Menu>
   );
 }
