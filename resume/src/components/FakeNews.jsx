af33d3b7f98f550290ed9d9b722c6ca119729ac9
Alexander Holmes <108624793+AlexanderHolmes0@users.noreply.github.com>
2024-04-06 16:04:29 -0400

    Gradio app

--- resume/src/components/FakeNews.jsx
+++ resume/src/components/FakeNews.jsx
@@ -2,7 +2,7 @@ function FakeNews() {
     return (
         <div className="appbox" >
             
-            <iframe src="https://alexanderholmes0-fake-news-detector-app.hf.space" frameborder="0" width="65%" height="850"></iframe>
+            <iframe src="https://alexanderholmes0-fake-news-detector-app.hf.space" frameborder="0" width="75%" height="850"></iframe>
 
         </div>
     );
