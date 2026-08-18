17ff0e1b2abdd5abf8dbed0587e210d752985fdd
Alexander Holmes <108624793+AlexanderHolmes0@users.noreply.github.com>
2026-08-17 23:36:49 -0400

    chore: update package.json to specify Node and npm engine versions; c… (#33)
    * chore: update package.json to specify Node and npm engine versions; change job title to Data Scientist in Home component; update GitHub stats image URLs
    
    * refactor: optimize component imports and enhance routing with lazy loading; rename Sidebar function for clarity; remove unused YoutubeDash component
    
    * chore: remove unused GIF files and update README with a new animated GIF

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/17cc2cc2-7246-43df-add3-5016f07f35d8/deploy-status)](https://app.netlify.com/sites/alexanderholmes/deploys)
 ![React](https://img.shields.io/badge/-React-000?&logo=React)
 --
-[![](https://github.com/AlexanderHolmes0/React_Resume/blob/main/Banner.gif)](https://alexanderholmes.netlify.app/)
+[![](https://media.giphy.com/media/PXVfszzMEUXFYmBPXU/giphy.gif)](https://alexanderholmes.netlify.app/)
 
-The inspiration for this project was Adam Alston. Thanks, Adam, for the great idea! 
+The inspiration for this project was Adam Alston. Thanks, Adam, for the great idea!
 
 <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Backhand%20Index%20Pointing%20Right%20Light%20Skin%20Tone.png" alt="Backhand Index Pointing Right Light Skin Tone" width="40" height="40" /> [Adam Alston GitHub](https://github.com/adamalston) <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Backhand%20Index%20Pointing%20Left%20Light%20Skin%20Tone.png" alt="Backhand Index Pointing Left Light Skin Tone" width="40" height="40" />
 
--- resume/src/Sidebar.jsx
+++ resume/src/Sidebar.jsx
@@ -4,7 +4,7 @@ import { bubble as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom'
 
 
-export default function menu() {
+export default function SidebarMenu() {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -49,12 +49,7 @@ export default function menu() {
       <h3> <img src='https://images.ctfassets.net/spoqsaf9291f/5M2QHtaWvuvEUU2V8MNVXH/f780593bab58709f95404d27bcb4e5d4/tableau-software.svg' height='40px'></img>
         <a className="nodeco" target="_blank" rel="noopener noreferrer" href='https://www.tableau.com/'> Tabelau Vizzes </a></h3>
       <hr />
-      <Link className="menu-item" to="KrogerDash" onClick={handleLinkClick}><span className='grayscale'>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Kroger_logo_%281961-2019%29.svg/200px-Kroger_logo_%281961-2019%29.svg.png' height='20px'></img>
-      </span>  Kroger</Link>
-      <Link className="menu-item" to="YoutubeDash" onClick={handleLinkClick}><span className='grayscale'>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1024px-YouTube_full-color_icon_%282017%29.svg.png' height='20px'></img>
-      </span> Youtube</Link>
+      <Link className="menu-item" to="KrogerDash" onClick={handleLinkClick}><span className='grayscale'> 🛒 </span>  Kroger</Link>
       <Link className="menu-item" to="KickstarterDash" onClick={handleLinkClick}><span className='grayscale'>
         <img src='https://cdn.icon-icons.com/icons2/2429/PNG/512/kickstarter_logo_icon_147271.png' height='25px'></img>
       </span> Kickstarter</Link>
